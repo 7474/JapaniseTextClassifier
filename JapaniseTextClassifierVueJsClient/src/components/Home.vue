40186a22a2ab66fea50293bb21f3bf54dacca719
koudenpa <koudenpa@hotmail.com>
2019-09-01 01:53:31 +0900

    openapi-generator typescript-fetch は不発だった予感。

--- JapaniseTextClassifierVueJsClient/src/components/Home.vue
+++ JapaniseTextClassifierVueJsClient/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="home">
+    <div class="home" v-on:click="onClick">
         <h1>{{ msg }}</h1>
         <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
     </div>
@@ -7,10 +7,28 @@
 
 <script lang="ts">
     import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { ClassificationApi, Configuration } from '../api';
 
     @Component
     export default class Home extends Vue {
         @Prop() private msg!: string;
+
+        // XXX APIクライアントの試し呼び
+        public async onClick() {
+            const client = new ClassificationApi(new Configuration({
+                // XXX 何処から食わせるのがいいの？
+                basePath: "http://localhost:7071/api",
+            }));
+            const result = await client.classifyJapaniseText({
+                body: {
+                    text: "これを分類できますか？",
+                    translatorName: "GcpTranslator",
+                    classifierName: "AzureClassifier",
+                },
+            });
+            console.log(result);
+            this.msg = "" + result.translatedText;
+        }
     }
 </script>
 
