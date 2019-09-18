254309c6864347b60f5b703f1f807da619a747a0
koudenpa <koudenpa@hotmail.com>
2019-09-18 10:13:24 +0900

    /.auth/refresh はあくまでIDプロバイダのトークン更新であってEasyAuthのセッション更新ではないようだった。
    TwitterのトークンでZUMO-AUTHを半永続的に取るお試しをしてみたところ想定通りにふるまいはした。

--- JapaniseTextClassifierVueJsClient/src/store/modules/id.ts
+++ JapaniseTextClassifierVueJsClient/src/store/modules/id.ts
@@ -33,6 +33,25 @@ class Id extends VuexModule {
     @Action({ commit: "setMe" })
     async getAuthMe(): Promise<Me | null> {
         // XXX 設定に切り出す
+        try {
+            const access_token = localStorage.getItem("twitter_access_token");
+            const access_token_secret = localStorage.getItem("twitter_access_token_secret");
+            // https://docs.microsoft.com/ja-jp/azure/app-service/app-service-authentication-how-to#extend-session-token-expiration-grace-period
+            const zumoResponse = await axios.post("https://japanisetextclassifierfunction.azurewebsites.net/.auth/login/twitter", {
+                access_token: access_token,
+                access_token_secret: access_token_secret
+            }, {
+                withCredentials: true,
+            });
+            axios.defaults.headers = {
+                ...axios.defaults.headers,
+                "X-ZUMO-AUTH": zumoResponse.data.authenticationToken,
+            }
+            console.log(zumoResponse);
+        } catch (err) {
+            console.log(err);
+        }
+        // refresh 結果は見ず単に /.auth/me を呼んでしまう
         const response = await axios.get("https://japanisetextclassifierfunction.azurewebsites.net/.auth/me", {
             withCredentials: true,
         });
@@ -40,6 +59,9 @@ class Id extends VuexModule {
 
         if (response.status === 200) {
             const data = response.data[0];
+            // XXX localStorage にトークン永続化は相当ダーティだと思うのでお試し。これするとほぼ永続セッションになりそう。
+            localStorage.setItem("twitter_access_token", data.access_token);
+            localStorage.setItem("twitter_access_token_secret", data.access_token_secret);
             return {
                 userId: data.user_id,
                 iconUrl: data.user_claims.find((x: any) => x.typ === "urn:twitter:profile_image_url_https").val,
