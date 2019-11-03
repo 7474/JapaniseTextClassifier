e38e663c6078d634c7e6a7e9964c2bcac371bcca
koudenpa <koudenpa@hotmail.com>
2019-11-04 00:51:47 +0900

    Nuxt.js と TypeScript 設定。

---
+++ JapaniseTextClassifierNuxtJsClient/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">JapaniseTextClassifierNuxtJsClient</h1>
+    </div>
+    <div>
+      <p>{{ helloMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+// import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    // Logo
+  },
+  data() {
+    return {
+      helloMessage: 'This is ts.'
+    }
+  }
+}
+</script>
+
+<style></style>
